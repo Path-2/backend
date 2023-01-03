24bbaffca1f27f1e338306a8fd589dcb59fa6ed7
DumiJDev <dumildesfernando@outlook.com>
2023-01-03 10:02:09 +0100

    initiliaze the project

---
+++ src/main/java/ao/path2/app/Path2AppApplication.java
@@ -0,0 +1,13 @@
+package ao.path2.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Path2AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Path2AppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ao/path2/app/Path2AppApplicationTests.java
@@ -0,0 +1,13 @@
+package ao.path2.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Path2AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
